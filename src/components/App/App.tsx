ef51e45270840e11ea35208e45077735770360ca
Andrey <antonov.andrey.10101@gmail.com>
2023-08-14 16:40:45 +0400

    feat: add AppShell

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import { useEffect, useState } from 'react';
-import { Center, Container, Flex, Loader, Pagination } from '@mantine/core';
+import { AppShell, Center, Container, Flex, Loader, Pagination } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
 import CharacterItem from '../CharacterItem/CharacterItem';
 import CharactersStore from '../../stores/CharactersStore';
@@ -12,18 +12,27 @@ const App = observer(() => {
   useEffect(() => {
     getAllCharacters(activePage);
   }, [activePage]);
+
   return (
-    <Container maw="1200px" p="20px 10px">
-      <Flex gap="20px" wrap="wrap" align="flex-end" justify="center" mb="30px">
-        {isLoading && <Loader size="xl" />}
-        {characters.map((character) => (
-          <CharacterItem key={character._id} character={character} />
-        ))}
-      </Flex>
-      <Center mx="auto">
-        <Pagination total={149} value={activePage} onChange={setPage} size="lg" radius="xl" />
-      </Center>
-    </Container>
+    <AppShell
+      styles={(theme) => ({
+        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+      })}
+    >
+      <Container maw="1200px" p="20px 10px">
+        <Flex gap="20px" wrap="wrap" align="flex-end" justify="center" mb="30px">
+          {isLoading && <Loader size="xl" />}
+        </Flex>
+        <Flex gap="20px" wrap="wrap" align="flex-end" justify="center" mb="30px">
+          {characters.map((character) => (
+            <CharacterItem key={character._id} character={character} />
+          ))}
+        </Flex>
+        <Center mx="auto">
+          <Pagination total={149} value={activePage} onChange={setPage} size="lg" radius="xl" />
+        </Center>
+      </Container>
+    </AppShell>
   );
 });
 
