75d158402785ff083bca4d56e196dca7f41e2498
Andrey <antonov.andrey.10101@gmail.com>
2023-08-14 15:06:55 +0400

    feat: add Card

--- src/components/CharacterItem/CharacterItem.tsx
+++ src/components/CharacterItem/CharacterItem.tsx
@@ -1,3 +1,4 @@
+import { Anchor, Badge, Button, Card, Flex, Group, Image, Space, Text } from '@mantine/core';
 import ICharacter from '../../types/character';
 import './CharacterItem.scss';
 
@@ -9,19 +10,36 @@ function CharacterItem({ character }: CharacterItemProps) {
   console.log(character);
 
   return (
-    <div className="character">
-      <img className="character__image" src={character.imageUrl} alt="Character" />
-      <div className="character__info">
-        <h2>Name: {character.name}</h2>
-        <h2>Films: {character.films.join(', ')}</h2>
-        <h2>
-          <a href={character.sourceUrl}>Source</a>
-        </h2>
-        <h2>
-          <a href={character.url}>Url</a>
-        </h2>
-      </div>
-    </div>
+      <Card maw='250px' shadow="sm" padding="lg" radius="md" withBorder w="100%">
+        <Card.Section>
+          <Image
+            src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+            height="160px"
+            alt="Norway"
+          />
+        </Card.Section>
+
+        <Group position="apart" mt="md" mb="xs">
+          <Text weight={500}>Name: {character.name}</Text>
+          <Flex gap="5px" justify="center" align="flex-start" wrap="wrap">
+          <Badge>
+            <Anchor href={character.sourceUrl} target="_blank" variant="light" color="blue">
+              Source
+            </Anchor>
+          </Badge>
+          <Badge>
+            <Anchor href={character.url} target="_blank" variant="light" color="blue">
+              Url
+            </Anchor>
+          </Badge>
+        </Flex>
+        </Group>
+
+        <Text size="sm" color="dimmed">
+          Films: {character.films.join(', ')}
+        </Text>
+
+      </Card>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App'
-import './main.scss'
+import { MantineProvider } from '@mantine/core';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
+import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <MantineProvider>
     <App />
-)
+  </MantineProvider>,
+);
